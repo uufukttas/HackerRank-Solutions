1e72ae39a541260942e9f5515607c77ee945fbd2
uufukttas <uufukttas@hotmail.com>
2022-01-03 23:51:13 +0300

    Solution | CountingSort1 Question.

---
+++ CountingSort1/solution.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr) {
+    // Write your code here
+    let output = [];
+
+    for (let index = 0; index < arr.length; index++) {
+        output[index] = 0;
+    };
+
+    for (let index = 0; index < arr.length; index++) {
+        if (output[(arr[index])] === 0) {
+            output[(arr[index])] = 1;
+        } else {
+            output[(arr[index])] = output[(arr[index])]+1;
+        }
+    };
+
+    return output;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = countingSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
